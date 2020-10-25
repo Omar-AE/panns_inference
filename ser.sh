b5363f4124c873228832fe16ec4153465ea56844
Omar-AE <o.alzaibag@acacusgroup.com>
2020-10-25 11:38:12 +0300

    added ser.service and edited ser.sh to kill the running python scripts when ctrl+c is pressed

--- ser.sh
+++ ser.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+ctrl_c() {
+        echo "Done";
+        kill -9 $P1
+	kill -9 $P2
+        exit;
+}
+
+trap ctrl_c SIGINT
+
+
 cd /home/nvr/panns_inference
 mkdir /home/nvr/airesults
 touch /home/nvr/airesults/ser.json
@@ -7,9 +17,13 @@ touch /home/nvr/airesults/ser.json
 mkdir /home/nvr/converted_audio_files/
 source /opt/intel/openvino/bin/setupvars.sh
 
-#taskset --cpu-list 1
-/usr/bin/python3 /home/nvr/panns_inference/convert.py &
+
+taskset --cpu-list 1 /usr/bin/python3 /home/nvr/panns_inference/convert.py &
 P1=$!
-/usr/bin/python3 /home/nvr/panns_inference/predict.py &
+taskset --cpu-list 1 /usr/bin/python3 /home/nvr/panns_inference/predict.py &
 P2=$!
-wait $P1 $P2
+
+while true  # to keep the bash file running so that pressing ctrl+c kills all running python scripts
+do
+	sleep 100
+done
